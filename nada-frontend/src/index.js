bf6bf0eb0d4f0d22b4f3e12f977c6b0d2e3f9fc9
dudgp128 <20181048@sungshin.ac.kr>
2023-04-10 20:57:01 +0900

    configureStore 사용하기

--- nada-frontend/src/index.js
+++ nada-frontend/src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './modules/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
