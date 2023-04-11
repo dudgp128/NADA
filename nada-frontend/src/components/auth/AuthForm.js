55666bf308a5e3bc2c54eed98e78b23530f45bbd
dudgp128 <20181048@sungshin.ac.kr>
2023-04-11 15:26:02 +0900

    AuthForm 기본틀 만들기

--- nada-frontend/src/components/auth/AuthForm.js
+++ nada-frontend/src/components/auth/AuthForm.js
@@ -1,6 +1,17 @@
 // 회원가입 또는 로그인 폼을 보여주기
 
+import Button from '../common/Button';
+
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  return (
+    <div>
+      <h3>로그인</h3>
+      <form>
+        <input name="username" placeholder="아이디" />
+        <input name="password" placeholder="비밀번호" type="password" />
+        <Button>로그인</Button>
+      </form>
+    </div>
+  );
 };
 export default AuthForm;
