51ee2e7096cdbe20878eb0c94a890b538d4cde3b
dudgp128 <20181048@sungshin.ac.kr>
2023-04-12 16:34:32 +0900

    [STYLE] 주석 삭제하기

--- nada-frontend/src/containers/auth/LoginForm.js
+++ nada-frontend/src/containers/auth/LoginForm.js
@@ -1,4 +1,3 @@
-//import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
 import { changeField, initializeForm } from '../../modules/auth';
